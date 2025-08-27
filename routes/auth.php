1a71362955052cf50320b50941b54d4ca257be54
kgcemon <kgcemonbd@gmail.com>
2025-08-27 09:53:27 -0700

    updated

--- routes/auth.php
+++ routes/auth.php
@@ -15,4 +15,5 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy']);
 });
