398803fcb5226c77c5009dfdcf44925d5350fe34
kgcemon <kgcemonbd@gmail.com>
2025-08-30 09:18:52 -0700

    updated

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -105,20 +105,24 @@
         <!-- Logo -->
         <a href="{{ url('/') }}" class="logo">{{ config('app.name', 'Codmshop') }}</a>
 
-        <!-- Auth Links -->
-        <div class="auth-link">
-            @auth
-                <a href="{{ url('/account') }}">My Account</a>
-{{--                <form method="POST" action="{{ route('logout') }}" style="display:inline;">--}}
-{{--                    @csrf--}}
-{{--                    <button type="submit" style="background:none;border:none;color:white;cursor:pointer;">Logout</button>--}}
-{{--                </form>--}}
-            @else
-                <a href="{{ url('/login') }}">Login</a>
-            @endauth
-        </div>
+        <!-- Wallet + Balance + Profile -->
+        <a href="{{ url('/account') }}" class="account-row">
+            <!-- Wallet Icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1"></path>
+                <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4"></path>
+            </svg>
+
+            <!-- Wallet Balance -->
+            <div class="wallet-balance">{{ Auth::user()->wallet ?? 0 }}৳</div>
+
+            <!-- Profile Image -->
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiU0_6Mf8AnR9ny0woh2-u7LcoB2oWrks8OpSQfhzA9xxfk9CL4oxNQnWjoxwkDJwwUnY&usqp=CAU" alt="user-profile-picture" class="profile-img">
+        </a>
     </div>
 </header>
+
+
 
 <!-- Main Content -->
 <main>
