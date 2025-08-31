ec8540e337ab830fd9859ccdee92dc7df40a208f
kgcemon <kgcemonbd@gmail.com>
2025-08-31 09:06:17 -0700

    updated

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -263,7 +263,7 @@
                     <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1"></path>
                     <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4"></path>
                 </svg>৳ {{ Auth::user()->wallet ?? 0 }}
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiU0_6Mf8AnR9ny0woh2-u7LcoB2oWrks8OpSQfhzA9xxfk9CL4oxNQnWjoxwkDJwwUnY&usqp=CAU" alt="user-profile-picture" class="profile-img">
+                <img src="{{ Auth::user()->image}}" alt="user-profile-picture" class="profile-img">
             </a>
         @else
             <!-- Login button for guests -->
