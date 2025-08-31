d376a96c967ff210998eef6180d27eea53032638
kgcemon <kgcemonbd@gmail.com>
2025-08-30 22:43:45 -0700

    updated

--- app/Http/Controllers/user/SiteHomeScreenController.php
+++ app/Http/Controllers/user/SiteHomeScreenController.php
@@ -4,21 +4,15 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use App\Models\Categorie;
-use Illuminate\Support\Facades\Cache;
 
 class SiteHomeScreenController extends Controller
 {
 
     public function index() {
-        $cacheKey = 'home_products';
-        Cache::forget($cacheKey);
-        $products = Cache::rememberForever($cacheKey, function() {
-            return Categorie::with('products')
-                ->select('name', 'id')
-                ->orderBy('sort')
-                ->paginate(50);
-        });
-
+        $products = Categorie::with('products')
+            ->select('name', 'id')
+            ->orderBy('sort')
+            ->paginate(50);
         return view('user.home', compact('products'));
     }
 
