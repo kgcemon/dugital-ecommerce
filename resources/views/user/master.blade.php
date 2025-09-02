0e0533d09c0840187abba2f425279a3d2315aedb
kgcemon <kgcemonbd@gmail.com>
2025-09-01 22:46:13 -0700

    updated

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -318,7 +318,7 @@
 
     @auth
         <!-- Logged in -->
-        <a href="{{ route('myOrders') }}" class="nav-item {{request('/my-orders') ? 'actv' : '' }}">
+        <a href="{{ route('myOrders') }}" class="nav-item {{ request()->routeIs('myOrders') ? 'actv' : '' }}">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M6 2h12l1 4H5l1-4zm0 6h14v14H6V8z"/>
             </svg>
