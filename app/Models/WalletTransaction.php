262443818daedd1476d1995518165c7c229c4e12
kgcemon <kgcemonbd@gmail.com>
2025-09-04 01:36:40 -0700

    updated

--- app/Models/WalletTransaction.php
+++ app/Models/WalletTransaction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class WalletTransaction extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'amount',
+        'type',
+        'description',
+        'status',
+    ];
 }
