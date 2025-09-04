0db8bb2df7272b61e9c8054d60bf28bb272bca90
kgcemon <kgcemonbd@gmail.com>
2025-09-04 09:52:36 -0700

    thank you page updated

--- resources/views/user/deposit.blade.php
+++ resources/views/user/deposit.blade.php
@@ -197,7 +197,7 @@
 
                         if (data.status) {
                             showToast("✅ ডিপোজিট সফল হয়েছে!", "success");
-                            window.location.href = "/thank-you/"+ data.order.uid;
+                            window.location.href = "/thank-you/"+order.uid;
                         } else {
                             paymentNumberBox.style.display = "block";
                             paymentNumberInput.focus();
