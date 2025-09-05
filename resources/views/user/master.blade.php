1d7de245de8e35e98a1740ead84c0def38444764
kgcemon <kgcemonbd@gmail.com>
2025-09-05 02:26:15 -0700

    slider images update fixed

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -378,7 +378,7 @@
 @auth
     <!-- Deposit Modal -->
     <div id="depositModal" class="google-login">
-        <div class="card" style="background: black">
+        <div class="card" style="background: black; padding: 10px">
             <span id="closeDeposit" class="close-btn">&times;</span>
             <h3 style="margin-bottom:15px;">Deposit Money</h3>
 
