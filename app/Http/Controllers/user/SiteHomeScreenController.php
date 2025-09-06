53653ec1c10ea0c4dcf2dcac8668874dc2a27755
kgcemon <kgcemonbd@gmail.com>
2025-09-06 04:59:59 -0700

    updated

--- app/Http/Controllers/user/SiteHomeScreenController.php
+++ app/Http/Controllers/user/SiteHomeScreenController.php
@@ -5,18 +5,27 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\Categorie;
 use App\Models\SliderImages;
+use Illuminate\Support\Facades\Cache;
 
 class SiteHomeScreenController extends Controller
 {
 
     public function index() {
-        $products = Categorie::with('products')
-            ->select('name', 'id')
-            ->orderBy('sort')
-            ->paginate(50);
-        $images = SliderImages::first();
+
+        $products = Cache::remember('home_products', 60 * 24, function () {
+            return Categorie::with('products')
+                ->select('name', 'id')
+                ->orderBy('sort')
+                ->paginate(50);
+        });
+
+        $images = Cache::remember('home_slider_images', 60 * 24, function () {
+            return SliderImages::first();
+        });
+
         return view('user.home', compact('products','images'));
     }
+
 
 
 
