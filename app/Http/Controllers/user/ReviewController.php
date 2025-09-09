5abc4f8a2eb158a3c657a9f1c7f5869947431a51
kgcemon <kgcemonbd@gmail.com>
2025-09-08 22:40:21 -0700

    updated

--- app/Http/Controllers/user/ReviewController.php
+++ app/Http/Controllers/user/ReviewController.php
@@ -12,7 +12,7 @@ class ReviewController extends Controller
     public function reviewByProduct($slug)
     {
         $product = Product::where('slug', $slug)->first();
-        $reviews = Review::where('slug', $product->id)->orderBy('created_at', 'desc')->paginate(20);
+        $reviews = Review::where('product_id', $product->id)->orderBy('created_at', 'desc')->paginate(20);
         return view('user.review', compact('reviews'));
     }
 
