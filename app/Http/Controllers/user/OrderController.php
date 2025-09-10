3c6e411cec3e99730115de32c51963a5ca8c9533
kgcemon <kgcemonbd@gmail.com>
2025-09-10 01:15:58 -0700

    updated

--- app/Http/Controllers/user/OrderController.php
+++ app/Http/Controllers/user/OrderController.php
@@ -103,10 +103,8 @@ class OrderController extends Controller
                 } else {
                     $paySMS = null;
                     if (!empty($validated['transaction_id'])) {
-                        $paySMS = PaymentSms::where('trxID', $validated['transaction_id'])
-                            ->where('amount', '>=', (integer)$item->price)
-                            ->where('status', 0)
-                            ->first();
+                        $paySMS = PaymentSms::where('trxID', $validated['transaction_id'])->where('status', 0)->first();
+
                         return $paySMS;
                     }
 
