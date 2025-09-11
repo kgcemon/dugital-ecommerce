26bd7ba4c83c91083be0a02774e05d341705341e
kgcemon <kgcemonbd@gmail.com>
2025-09-10 21:29:50 -0700

    updated

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -22,7 +22,7 @@
                     </div>
                 @endif
                     @if (session('error'))
-                        <div class="alert alert-danger alert-dismissible fade show">{{ session('success') }}
+                        <div class="alert alert-danger alert-dismissible fade show">{{ session('error') }}
                             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                         </div>
                     @endif
