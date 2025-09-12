36d9b1d341453f7ed2aba494995020ad3b2f49fd
kgcemon <kgcemonbd@gmail.com>
2025-09-12 08:46:53 -0700

    Initial commit

--- resources/views/admin/pages/codes/codes.blade.php
+++ resources/views/admin/pages/codes/codes.blade.php
@@ -40,7 +40,7 @@
                                     </strong>
                                 </td>
                                 <td>
-                                    <a href="{{ route('admin.codes.edit', $row->variant->denom) }}">View</a>
+                                    <a href="{{ route('codes.edit', $row->id) }}">View</a>
                                 </td>
                             </tr>
                         @empty
