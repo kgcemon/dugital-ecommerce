9824f629f3cef9a79023eac0699b001a38df3550
kgcemon <kgcemonbd@gmail.com>
2025-09-17 00:57:22 -0700

    like added

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -280,7 +280,7 @@
 
 
 @if (Request::is('/'))
-        <link rel="stylesheet" href="{{ asset('assets/user/home.css?v=136') }}">
+        <link rel="stylesheet" href="{{ asset('assets/user/home.css?v=137') }}">
     @endif
 
     @if (Request::is('product*') || Request::is('thank-you*') || Request::is('deposit*'))
