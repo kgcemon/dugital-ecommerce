92c1c3ed0099b27d2378b116a3555da467c1c95b
kgcemon <kgcemonbd@gmail.com>
2025-09-20 09:38:49 -0700

    first commit

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,33 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 class SitemapController extends Controller
 {
     public function index()
     {
-        $urls = [
-            [
-                'loc' => url('/'),
-                'lastmod' => now()->toAtomString(),
-                'changefreq' => 'daily',
-                'priority' => '1.0'
-            ],
-            [
-                'loc' => url('/about'),
-                'lastmod' => now()->toAtomString(),
-                'changefreq' => 'monthly',
-                'priority' => '0.8'
-            ],
-        ];
-
         $sitemaps = [
             url('/sitemap-products.xml'),
         ];
 
-        $content = view('sitemap', compact('urls', 'sitemaps'));
+        $content = view('sitemap-index', compact('sitemaps'));
 
         return response($content, 200)
             ->header('Content-Type', 'application/xml');
@@ -37,16 +21,19 @@ class SitemapController extends Controller
 
     public function products()
     {
-        $urls = Product::where('name', '!=', 'Wallet')
-            ->get()
-            ->map(function ($product) {
-                return [
-                    'loc' => url('/product/' . $product->slug),
-                    'lastmod' => $product->updated_at->toAtomString(),
-                    'changefreq' => 'weekly',
-                    'priority' => '0.8'
-                ];
-            });
+        // 2 দিনের জন্য cache করা
+        $urls = Cache::remember('sitemap_products', now()->addDays(2), function () {
+            return Product::where('name', '!=', 'Wallet')
+                ->get()
+                ->map(function ($product) {
+                    return [
+                        'loc' => url('/product/' . $product->slug),
+                        'lastmod' => $product->updated_at->toAtomString(),
+                        'changefreq' => 'weekly',
+                        'priority' => '0.8'
+                    ];
+                });
+        });
 
         $content = view('sitemap', ['urls' => $urls]);
 
